a21d5fa222e82e63871c0840bc751a10804fd24d
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-06-05 16:45:49 -0300

    up

--- Java/Projeto2_1b/JogoDaMemoria/Logica/ProgramaPrincipal.java
+++ Java/Projeto2_1b/JogoDaMemoria/Logica/ProgramaPrincipal.java
@@ -2,4 +2,9 @@ package Projeto2_1b.JogoDaMemoria.Logica;
 
 public class ProgramaPrincipal {
     
+    public static void main(String [] args){
+     
+        new JogoDaMemoria();
+     
+    } 
 }
