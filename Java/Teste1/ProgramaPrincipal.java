5c92d172c9c7b507454453b081bcf8078b316d0e
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-06-07 22:03:56 -0300

    up

---
+++ Java/Teste1/ProgramaPrincipal.java
@@ -0,0 +1,8 @@
+package Teste1;
+
+public class ProgramaPrincipal {
+    public static void main(String[] args) {
+        FrmJogo frmJogo = new FrmJogo();
+        frmJogo.setVisible(true);
+    }
+}
